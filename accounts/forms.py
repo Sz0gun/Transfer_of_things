4b8dc2d9be9c7ad8eb3ba09b7438eea179b0e3d0
Sz0gun <mks.viunov@gmail.com>
2023-11-06 21:00:04 +0100

    accounts_forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,12 +5,15 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserAdminCreationForm(UserCreationForm):
-    """A custom form for creation new users."""
+    """A custom form for creating new users."""
 
-    # required_css_class = ...
+    required_css_class = "form-group"
     first_name = forms.CharField()
     last_name = forms.CharField()
 
     class Meta:
         model = get_user_model()
         fields = ['email', 'first_name', 'last_name']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-group'}),
+        }
